b8bc0e901648810219b68e10b8a2b2af924466e4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-05 20:02:54 +0800

    feat: add searchParam

---
+++ src/transformers/search-param.ts
@@ -0,0 +1,15 @@
+import { HTTPOptions, HTTPOptionsTransformer } from '@src/types'
+
+export function searchParam(name: string, value: string): HTTPOptionsTransformer {
+  return (options: HTTPOptions) => {
+    const url = new URL(options.url.href)
+    const newSearchParams = new URLSearchParams(url.searchParams)
+    newSearchParams.append(name, value)
+    url.search = newSearchParams.toString()
+
+    return {
+      ...options
+    , url
+    }
+  }
+}
