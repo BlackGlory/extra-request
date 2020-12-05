12d538a39a86558cef0c041a36c8657d73a51d37
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-05 20:03:14 +0800

    feat: modify searchParams
    BREAKING CHANGE: searchParams becomes append only

--- src/transformers/search-params.ts
+++ src/transformers/search-params.ts
@@ -3,7 +3,11 @@ import { HTTPOptions, HTTPOptionsTransformer } from '@src/types'
 export function searchParams(searchParams: { [name: string]: string }): HTTPOptionsTransformer {
   return (options: HTTPOptions) => {
     const url = new URL(options.url.href)
-    url.search = new URLSearchParams(searchParams).toString()
+    const newSearchParams = new URLSearchParams(url.searchParams)
+    for (const [name, value] of Object.entries(searchParams)) {
+      newSearchParams.append(name, value)
+    }
+    url.search = newSearchParams.toString()
 
     return {
       ...options
