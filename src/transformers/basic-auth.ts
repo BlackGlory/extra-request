17c9a1c3aafb1a17b6f1ece1654758d078cf01bf
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-06-09 08:51:38 +0000

    refactor: use @blackglory/jest-resolver

--- src/transformers/basic-auth.ts
+++ src/transformers/basic-auth.ts
@@ -1,8 +1,5 @@
 import { header } from './header.js'
 import { IHTTPOptionsTransformer } from '@src/types.js'
-import * as base64 from 'js-base64'
-
-const { btoa } = base64
 
 export function basicAuth(username: string, password: string): IHTTPOptionsTransformer {
   return header('Authorization', 'Basic ' + btoa(`${username}:${password}`))
