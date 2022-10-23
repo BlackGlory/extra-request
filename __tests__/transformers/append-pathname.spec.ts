e31c9181eac10f62ba16834b955ba0c1dcc031a4
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-10-23 11:04:36 +0000

    test: add cases

--- __tests__/transformers/append-pathname.spec.ts
+++ __tests__/transformers/append-pathname.spec.ts
@@ -2,10 +2,26 @@ import { appendPathname } from '@transformers/append-pathname.js'
 import { createOptions } from './utils.js'
 
 describe('appendPathname', () => {
+  test('pathname starts with slash', () => {
+    const options = createOptions({ pathname: '/pathname/' })
+
+    const result = appendPathname('/test')(options)
+
+    expect(result.url.pathname).toBe('/pathname/test')
+  })
+
+  test('pathname does not start with slash', () => {
+    const options = createOptions({ pathname: '/pathname/' })
+
+    const result = appendPathname('test')(options)
+
+    expect(result.url.pathname).toBe('/pathname/test')
+  })
+
   test('url does not end with slash', () => {
     const options = createOptions({ pathname: '/pathname' })
 
-    const result = appendPathname('/test')(options)
+    const result = appendPathname('test')(options)
 
     expect(result.url.pathname).toBe('/pathname/test')
   })
@@ -13,7 +29,7 @@ describe('appendPathname', () => {
   test('url ends with slash', () => {
     const options = createOptions({ pathname: '/pathname/' })
 
-    const result = appendPathname('/test')(options)
+    const result = appendPathname('test')(options)
 
     expect(result.url.pathname).toBe('/pathname/test')
   })
