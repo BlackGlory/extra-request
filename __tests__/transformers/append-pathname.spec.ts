8cc898a660e760c20d60e32a3616e58650f6ba9b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-10-23 11:12:01 +0000

    fix: edge cases for `appendPathname`

--- __tests__/transformers/append-pathname.spec.ts
+++ __tests__/transformers/append-pathname.spec.ts
@@ -2,35 +2,63 @@ import { appendPathname } from '@transformers/append-pathname.js'
 import { createOptions } from './utils.js'
 
 describe('appendPathname', () => {
-  test('pathname starts with slash', () => {
-    const options = createOptions({ pathname: '/pathname/' })
+  describe('pathname', () => {
+    test('pathname starts with slash', () => {
+      const options = createOptions({ pathname: '/pathname/' })
 
-    const result = appendPathname('/test')(options)
+      const result = appendPathname('/test')(options)
 
-    expect(result.url.pathname).toBe('/pathname/test')
+      expect(result.url.pathname).toBe('/pathname/test')
+    })
+
+    test('pathname does not start with slash', () => {
+      const options = createOptions({ pathname: '/pathname/' })
+
+      const result = appendPathname('test')(options)
+
+      expect(result.url.pathname).toBe('/pathname/test')
+    })
   })
 
-  test('pathname does not start with slash', () => {
-    const options = createOptions({ pathname: '/pathname/' })
+  describe('url.pathname', () => {
+    test('url.pathname does not end with slash', () => {
+      const options = createOptions({ pathname: '/pathname' })
+
+      const result = appendPathname('test')(options)
+
+      expect(result.url.pathname).toBe('/pathname/test')
+    })
+
+    test('url.pathname ends with slash', () => {
+      const options = createOptions({ pathname: '/pathname/' })
+
+      const result = appendPathname('test')(options)
+
+      expect(result.url.pathname).toBe('/pathname/test')
+    })
+  })
+
+  test('url.search exists', () => {
+    const options = createOptions({
+      pathname: '/pathname/'
+    , search: '?name=value'
+    })
 
     const result = appendPathname('test')(options)
 
     expect(result.url.pathname).toBe('/pathname/test')
+    expect(result.url.search).toBe('?name=value')
   })
 
-  test('url does not end with slash', () => {
-    const options = createOptions({ pathname: '/pathname' })
+  test('url.hash exists', () => {
+    const options = createOptions({
+      pathname: '/pathname/'
+    , hash: '#hash'
+    })
 
     const result = appendPathname('test')(options)
 
     expect(result.url.pathname).toBe('/pathname/test')
-  })
-
-  test('url ends with slash', () => {
-    const options = createOptions({ pathname: '/pathname/' })
-
-    const result = appendPathname('test')(options)
-
-    expect(result.url.pathname).toBe('/pathname/test')
+    expect(result.url.hash).toBe('#hash')
   })
 })
--- __tests__/transformers/utils.ts
+++ __tests__/transformers/utils.ts
@@ -2,13 +2,21 @@ import { Headers } from 'extra-fetch'
 import { IRequestOptions } from '@src/types.js'
 
 export function createOptions(
-  { pathname }: {
+  { pathname, search, hash }: {
     pathname?: string
+    search?: string
+    hash?: string
   } = {}
 ): IRequestOptions {
   const url = new URL('protocol://localhost')
   if (pathname) {
     url.pathname = pathname
+  }
+  if (search) {
+    url.search = search
+  }
+  if (hash) {
+    url.hash = hash
   }
 
   return {
