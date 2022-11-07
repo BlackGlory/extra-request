042368744a48578da6d4742ce70b4c0ec594a61c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-07 07:51:41 +0000

    refactor: use extra-utils

--- src/pipe-request-options-transformers.ts
+++ src/pipe-request-options-transformers.ts
@@ -1,16 +1,19 @@
 import { Headers } from 'extra-fetch'
 import { IRequestOptions, IRequestOptionsTransformer } from '@src/types.js'
-import { isFunction } from '@blackglory/prelude'
+import { isntFalsy } from '@blackglory/prelude'
 import { Falsy } from '@blackglory/prelude'
+import { pipe } from 'extra-utils'
 
 export function pipeRequestOptionsTransformers(
   ...transformers: Array<IRequestOptionsTransformer | Falsy>
 ): IRequestOptions {
-  return transformers.reduce<IRequestOptions>(
-    (options, transformer) => isFunction(transformer) ? transformer(options) : options
-  , {
-      url: new URL('http://localhost')
-    , headers: new Headers()
-    }
+  const base: IRequestOptions = {
+    url: new URL('http://localhost')
+  , headers: new Headers()
+  }
+
+  return pipe(
+    base
+  , ...transformers.filter<IRequestOptionsTransformer>(isntFalsy)
   )
 }
