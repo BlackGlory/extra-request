e28652213eccd3a07c0d4eec85cca6e2689fab32
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-11-13 06:10:40 +0000

    test: improve cases

--- __tests__/transformers/csv.spec.ts
+++ __tests__/transformers/csv.spec.ts
@@ -1,14 +1,41 @@
 import { createOptions } from './utils.js'
 import { csv } from '@transformers/csv.js'
 
-test('csv<T extends object>(payload: T[]): RequestOptionsTransformer', () => {
+test('csv', () => {
   const options = createOptions()
   const payload = [
-    { key: 'hello', value: 'world' }
+    {
+      key: 'foo'
+    , value: 'bar'
+    }
+  , {
+      key: '"foo"'
+    , value: '"bar"'
+    }
+  , {
+      key: '"foo'
+    , value: 'bar"'
+    }
+  , {
+      key: '\nfoo'
+    , value: 'bar\n'
+    }
+  , {
+      key: ',foo'
+    , value: 'bar,'
+    }
   ]
 
   const result = csv(payload)(options)
 
+  // RFC 4180 and MIME standards
   expect(result.headers.get('Content-Type')).toBe('text/csv')
-  expect(result.payload).toBe('key,value\r\nhello,world')
+  expect(result.payload).toBe(
+    'key,value' + '\r\n'
+  + 'foo,bar' + '\r\n'
+  + '"""foo""","""bar"""' + '\r\n'
+  + '"""foo","bar"""' + '\r\n'
+  + '"\nfoo","bar\n"' + '\r\n'
+  + '",foo","bar,"'
+  )
 })
